66cd9410874b16b920bdec95f03e5388516680ca
Alisher Gani <ka4anbek@gmail.com>
2021-01-22 14:58:59 +0600

    Recurtional render of TreeItem.js

--- src/components/TreeItem.js
+++ src/components/TreeItem.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function TreeItem(props) {
+    let treeItems;
+    if (!!props.item.inner) {
+        treeItems = props.item.inner.map((item, i) =>
+            <TreeItem key={i} item={item}/>
+        );
+    }
+
+
     return (
         <div className="item">
             +{props.item.title}
+            <div>
+                {treeItems}
+            </div>
         </div>
     );
 }
